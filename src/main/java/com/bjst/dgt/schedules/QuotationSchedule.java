b5d0cacacdbabd93c82074fe348596a86510ce08
zhaoleiluo <zhaoleiluo@cehk.vip>
2018-06-14 15:39:52 +0800

    行情定时器

---
+++ src/main/java/com/bjst/dgt/schedules/QuotationSchedule.java
@@ -0,0 +1,31 @@
+package com.bjst.dgt.schedules;
+
+import com.bjst.dgt.service.StockDatasService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.Random;
+
+/**
+ * Created by zll on 2018/6/14.
+ * 行情定时器
+ */
+@Component
+@EnableScheduling
+public class QuotationSchedule {
+
+    @Autowired
+    private StockDatasService stockDatasService;
+
+    /**
+     * 每秒一次，周一到周六
+     */
+    @Scheduled(cron = "1/1 * * * * 2-7")
+    public void quotationTimer() {
+        System.out.println("here" + new Random().nextInt(1000));
+        stockDatasService.getProdects();
+    }
+}
